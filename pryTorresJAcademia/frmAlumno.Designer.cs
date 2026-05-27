e96ea11af0a47048c059df5d754eb2e295890aea
mieludo <joaquintttorres@hotmail.com>
2026-05-27 09:59:19 -0300

    nuevo form

---
+++ pryTorresJAcademia/frmAlumno.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace pryTorresJAcademia
+{
+    partial class frmAlumno
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmAlumno
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 385);
+            this.Name = "frmAlumno";
+            this.Text = "frmAlumno";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
